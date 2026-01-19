b476b1ce85e1f4eb6d74b76ec5a8ac171a5270ce
lcpnzz <lcpnzz@users.noreply.github.com>
2026-01-19 08:53:39 +0100

    Add files via upload

---
+++ HHH_deconstruction/HHH_diagonalCKM_4FNS_NLO_UFO/coupling_orders.py
@@ -0,0 +1,33 @@
+# This file was automatically created by FeynRules 2.3.49
+# Mathematica version: 13.3.0 for Linux x86 (64-bit) (June 3, 2023)
+# Date: Thu 4 Dec 2025 09:20:25
+
+
+from object_library import all_orders, CouplingOrder
+
+
+HBBMOD = CouplingOrder(name = 'HBBMOD',
+                       expansion_order = 99,
+                       hierarchy = 2)
+
+HHH = CouplingOrder(name = 'HHH',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
+HHHH = CouplingOrder(name = 'HHHH',
+                     expansion_order = 99,
+                     hierarchy = 2)
+
+HTTMOD = CouplingOrder(name = 'HTTMOD',
+                       expansion_order = 99,
+                       hierarchy = 2)
+
+QCD = CouplingOrder(name = 'QCD',
+                    expansion_order = 99,
+                    hierarchy = 1,
+                    perturbative_expansion = 1)
+
+QED = CouplingOrder(name = 'QED',
+                    expansion_order = 99,
+                    hierarchy = 2)
+
